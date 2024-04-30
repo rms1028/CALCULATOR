da8e8200fe6b30ba06a9ebf49c87fc9846d90fc7
rms1028 <rmsdnjs1028@naver.com>
2024-04-30 13:49:46 +0900

    onclick number

--- main.js
+++ main.js
@@ -3,3 +3,13 @@ let result = 0;
 const resultEl = document.querySelector(".result");
 
 resultEl.innerHTML = result;
+
+function onClickNumber(number) {
+  if (resultEl.innerHTML.length >= 13) return;
+
+  if (resultEl.innerHTML === "0") {
+    resultEl.innerHTML = number;
+  } else {
+    resultEl.innerHTML += number;
+  }
+}
