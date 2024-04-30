3e598722d55f1b323f9b5e93e069d6a0d9f347e8
rms1028 <rmsdnjs1028@naver.com>
2024-04-30 17:24:25 +0900

    onclickOperator

--- main.js
+++ main.js
@@ -5,6 +5,16 @@ const resultEl = document.querySelector(".result");
 
 resultEl.innerHTML = result;
 
+function onClickNumber(number) {
+  if (resultEl.innerHTML.length >= 13) return;
+
+  if (resultEl.innerHTML === "0") {
+    resultEl.innerHTML = number;
+  } else {
+    resultEl.innerHTML += number;
+  }
+}
+
 function onClickOperator(inputOperator) {
   result = +resultEl.innerHTML;
 
@@ -13,12 +23,36 @@ function onClickOperator(inputOperator) {
   operator = inputOperator;
 }
 
-function onClickNumber(number) {
-  if (resultEl.innerHTML.length >= 13) return;
+function onClickEnter() {
+  switch (operator) {
+    case "+":
+      result += +resultEl.innerHTML;
+      resultEl.innerHTML = result;
+      break;
+    case "-":
+      result -= +resultEl.innerHTML;
+      resultEl.innerHTML = result;
+      break;
+    case "*":
+      result *= +resultEl.innerHTML;
+      resultEl.innerHTML = result;
+      break;
+    case "/":
+      result = parseInt(result / +resultEl.innerHTML, 10);
+      resultEl.innerHTML = result;
+      break;
+    default:
+      console.log("잘못된 연산자입니다.");
+  }
+}
 
-  if (resultEl.innerHTML === "0") {
-    resultEl.innerHTML = number;
-  } else {
-    resultEl.innerHTML += number;
+function onClickDelete() {
+  resultEl.innerHTML = resultEl.innerHTML.substring(
+    0,
+    resultEl.innerHTML.length - 1
+  );
+
+  if (resultEl.innerHTML.length === 0) {
+    resultEl.innerHTML = 0;
   }
 }
