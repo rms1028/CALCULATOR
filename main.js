00d33637fe5e9b262c9971fa6e575e18ca6159f5
rms1028 <rmsdnjs1028@naver.com>
2024-04-30 14:45:10 +0900

    onclikOperator

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 let result = 0;
+let operator = "";
 
 const resultEl = document.querySelector(".result");
 
 resultEl.innerHTML = result;
+
+function onClickOperator(inputOperator) {
+  result = +resultEl.innerHTML;
+
+  resultEl.innerHTML = 0;
+
+  operator = inputOperator;
+}
 
 function onClickNumber(number) {
   if (resultEl.innerHTML.length >= 13) return;
